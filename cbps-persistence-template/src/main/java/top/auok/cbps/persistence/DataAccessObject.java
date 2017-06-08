70947107f99307fdfb14b33608820fc5b5ce8a48
Alex Lee <leocnx@163.com>
2017-06-08 10:58:29 +0800

    Add persistence template interface

---
+++ cbps-persistence-template/src/main/java/top/auok/cbps/persistence/DataAccessObject.java
@@ -0,0 +1,25 @@
+package top.auok.cbps.persistence;
+
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * persistence template interface
+ *
+ * @param <T>
+ * @param <K>
+ */
+public interface DataAccessObject<T, K> {
+
+	void create(T entity);
+
+	T findById(K key, String... fetchProfiles);
+
+	<C extends Collection<? super T>> C findByIds(Set<K> keys, C receivingList);
+
+	<C extends Collection<? super T>> C findAll(C receivingList);
+
+	T update(T entity);
+
+	void delete(K key);
+}
