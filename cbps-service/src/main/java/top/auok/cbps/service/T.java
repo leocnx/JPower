e4322a7c1555961087d2b95a8e8de96e0fa07ebc
Alex Lee <leocnx@163.com>
2017-06-24 17:01:02 +0800

    Add IdGenerator written by twitter

--- cbps-service/src/main/java/top/auok/cbps/service/T.java
+++ cbps-service/src/main/java/top/auok/cbps/service/T.java
@@ -1,5 +1,12 @@
 package top.auok.cbps.service;
 
+import top.auok.cbps.service.util.IdGenerator;
+
 public class T {
+
+	public static void main(String[] args) {
+		System.out.println(IdGenerator.getFlowId());
+		System.out.println(IdGenerator.getCommonId());
+	}
 
 }
